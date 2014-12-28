4b42bf202569a7708a23bd49851aa68ea357419e
Anand A Chokshi <a_chokshi@hotmail.com>
2014-12-27 23:03:04 -0800

    Candy

--- src/project_1_final/core.clj
+++ src/project_1_final/core.clj
@@ -7,5 +7,5 @@
   (def z (+ (/ c3 2) (/ c2 2)))                                                                ;; Variable z = (last/2 + second/2)
       (if(odd? x) (def x (+ x 1))) (if(odd? y) (def y (+ y 1))) (if(odd? z) (def z (+ z 1))))  ;; Check wheather candy is odd or not, if odd add one
 (def candy [x y z])                                                                            ;; generate new vector for manipulation in next turn
-(if (= x y z) (print (conj result [x y z i]))                                                  ;; if to check wheather three student have same candy
+(if (= x y z) (print (conj result [x y z i]))                                                 ;; if to check wheather three student have same candy
   (recur (conj result [x y z i]) (inc i)))))                                                   ;; add current data to result so we can finally print result with all the records
